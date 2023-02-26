98537f31e37d09099c761ce16dde11eea537688d
Henry Liu <yangyang8599@163.com>
2023-02-27 00:10:13 +0800

    Update drun_mysql_pe.sh

--- mysql/drun_mysql_pe.sh
+++ mysql/drun_mysql_pe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#生产mysql
+#生产mysql，#5.7的my.cnf 在/etc/下
 docker run -d -e MYSQL_ROOT_PASSWORD=AD4miniantsPE -p 13306:3306 -v /data/mysql_pe:/var/lib/mysql \
  -v /data/mysql_pe/my.cnf:/etc/mysql/my.cnf \
  --restart=on-failure \
